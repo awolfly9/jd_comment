3fee1067b191b61be57e423c16550db3256a494f
lgq <lgq@MFHJ-DZ-001-399deMacBook-Pro.local>
2017-04-14 15:17:46 +0800

    修改配置

--- config.py
+++ config.py
@@ -1,43 +1,22 @@
 # coding=utf-8
 
 # local
-# database_config = {
-#     'host': 'localhost',
-#     'port': 3306,
-#     'user': 'root',
-#     'password': '123456',
-#     'charset': 'utf8',
-# }
-#
-# database = 'jd'
-# jd_item_table = 'item'
-#
-# redis_pass = ''
-# redis_host = 'localhost'
-# redis_part = '6379'
-# redis_db = 10
-#
-# domain = 'http://127.0.0.1:8000'
-#
-# process_count = 3
-
-# aliyun
 database_config = {
-    'host': '101.200.55.192',
+    'host': 'localhost',
     'port': 3306,
     'user': 'root',
-    'password': 'JustYouself.9',
+    'password': '123456',
     'charset': 'utf8',
 }
 
 database = 'jd'
 jd_item_table = 'item'
 
-redis_pass = 'JustYouself.9'
-redis_host = '101.200.55.192'
+redis_pass = ''
+redis_host = 'localhost'
 redis_part = '6379'
 redis_db = 10
 
-domain = 'http://awolfly9.com/'
+domain = 'http://127.0.0.1:8000/'
 
 process_count = 3
